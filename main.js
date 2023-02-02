3353c5fcc5f1ce26b9646c7c2514b44592adba82
cwjohnson7 <cliffj1230@gmail.com>
2023-02-01 23:14:48 -0500

    Initial Commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// your code here
+var button = document.getElementsByTagName('button')[0];
+
+button.addEventListener('click', function () {
+  var userNameInput = document.getElementById('name')[0].value;
+  var userPostInput = document.getElementById('message')[0].value;
+  var pName = document.createElement('p');
+  var pPost = document.createElement('p');
+  var userNameInputElement = document.createTextNode(userNameInput);
+  var userPostInputElement = document.createTextNode(userPostInput);
+  
+  pPost.appendChild(userPostInputElement)
+  pName.appendChild(userNameInputElement);
+  
+  document.getElementsByClassName('posts')[0].append(pName);
+  document.getElementsByClassName('posts')[0].append(pPost);
+});
